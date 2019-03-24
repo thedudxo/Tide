bc0ede59c50da5c2ce3814365eaffb5cc3790dd0
thedudxo <Isaacboon@gmail.com>
2019-03-24 19:18:15 +1300

    Selection box is hard

---
+++ Assets/Project/Scripts/SelectionBox.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionBox : MonoBehaviour
+{
+
+    [SerializeField] GameObject selectionIndicator;
+    [SerializeField] Camera cam;
+    int selectButton = 0;
+
+    Vector2 startSelectPos,
+            mouseWorldPos;
+    Vector3 topLeft;
+
+    [SerializeField] public float gridSize;
+
+    private void OnGUI()
+    {
+        //Get the mouse position in world space
+        Event currentEvent = Event.current;
+        Vector3 currentMousePos = new Vector2(
+            currentEvent.mousePosition.x,
+            cam.pixelHeight - currentEvent.mousePosition.y); //y position is inverted.
+
+        mouseWorldPos = cam.ScreenToWorldPoint(new Vector3(
+            currentMousePos.x, 
+            currentMousePos.y,
+            -cam.transform.position.z));
+
+        topLeft = selectionIndicator.transform.position - (selectionIndicator.transform.localScale /2);
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButton(selectButton))
+        {
+            if (Input.GetMouseButtonDown(selectButton))
+            {
+                startSelectPos = new Vector3(Mathf.Round(mouseWorldPos.x), Mathf.Round(mouseWorldPos.y)); //round to an interger
+                selectionIndicator.transform.position = startSelectPos;
+                selectionIndicator.transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            //round to grid, and always be under the mouse
+            float dragPosX, dragPosY;
+            if (mouseWorldPos.x > startSelectPos.x)
+                { dragPosX = Mathf.Ceil(mouseWorldPos.x); Debug.Log("1"); } else
+                { dragPosX = Mathf.Floor(mouseWorldPos.x); Debug.Log("2"); }
+            if (mouseWorldPos.y > startSelectPos.y)
+                { dragPosY = Mathf.Ceil(mouseWorldPos.y); Debug.Log("8"); } else
+                { dragPosY = Mathf.Floor(mouseWorldPos.y); Debug.Log("9"); }
+            Vector3 dragPosition = new Vector3(dragPosX, Mathf.Ceil(mouseWorldPos.y));
+
+            //move cube to where you clicked
+            selectionIndicator.transform.position = startSelectPos;
+
+            //Vector3 distance = dragPosition - topLeft;
+            //selectionIndicator.transform.localScale = new Vector3(distance.x, distance.y, 1) ;
+            //selectionIndicator.transform.position += selectionIndicator.transform.localScale / 2;
+        }
+
+    }
+}
---
+++ Assets/Project/Scripts/TimeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour {
+
+    [SerializeField] Text dayText;
+    [SerializeField] float dayLength;
+    float elapsedTimeToday = 0;
+    int currentDay = 0;
+
+    // ///////////////////////////////////////////
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        elapsedTimeToday += Time.deltaTime;
+        if(elapsedTimeToday > dayLength)  { NewDay(); }
+	}
+
+    void NewDay()
+    {
+        elapsedTimeToday = 0;
+        currentDay++;
+        dayText.text = "Day " + currentDay;
+    }
+}
